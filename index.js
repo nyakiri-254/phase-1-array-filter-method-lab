adff6534b1c171394bb7410d9b6dae4f5c1b6241
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-12-03 20:59:55 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, sought) {
+return source.filter (
+    (possiblematch) => possiblematch.toLowerCase() === sought.toLowerCase()
+
+);
+
+}
+
+
+function fuzzyMatch(drivers, testString) {
+    return source.filter(
+        (possibleMatch) =>
+        possibleMatch.toLower().indexof(testString.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, soughtName) {
+    return driver.filter((record) => record.name === soughName);
+}
